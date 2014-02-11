75f226d0410ece5bea985c008d2732c1c2c9d008
NoiseDoll <bolya2003@bk.ru>
2014-02-11 14:31:27 +0000

    pre-sound functions (position and animation)

--- main/src/com/miloshpetrov/sol2/soundtest/SoundTestCmp.java
+++ main/src/com/miloshpetrov/sol2/soundtest/SoundTestCmp.java
@@ -2,10 +2,10 @@ package com.miloshpetrov.sol2.soundtest;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.math.Vector2;
 import com.miloshpetrov.sol2.Const;
-import com.miloshpetrov.sol2.common.Col;
 import com.miloshpetrov.sol2.ui.UiDrawer;
 
 class SoundTestCmp {
@@ -13,6 +13,11 @@ class SoundTestCmp {
 
   private final UiDrawer myUiDrawer;
   private final Vector2 myPos = new Vector2(.5f, .5f);
+  private Color c = new Color(myPos.x, myPos.y, 0, 1f);
+  private boolean start = false;
+  private float play_time = 0f;
+  private float delta = 0f;
+  private float radius = .05f;
 
   private float myAccum;
 
@@ -22,7 +27,8 @@ class SoundTestCmp {
 
   // this method is called externally as often as possible
   public void render() {
-    myAccum += Gdx.graphics.getDeltaTime();
+    delta = Gdx.graphics.getDeltaTime();
+    myAccum += delta;
     // we want to call the update() method 60 times per second or so, therefore these checks are needed
     while (myAccum > Const.REAL_TIME_STEP) {
       // in this method we update the game state
@@ -37,13 +43,28 @@ class SoundTestCmp {
     // clearing the screen from the previous frame
     Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
     myUiDrawer.begin();
-    myUiDrawer.drawCircle(myPos, .05f, Col.W);
+    myUiDrawer.drawCircle(myPos, radius, c);
     myUiDrawer.end();
   }
 
   private void update() {
     updatePos();
     updateSound();
+    change_color();
+    if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) //if pressed mouse button - init circle auto-morphing
+    {
+      start = true;
+      play_time = 0f;
+      radius = .05f;
+    }
+    if (start && play_time <= 1f) //full animation lasts 1 second
+    {
+      change_radius(delta);
+    }
+    else
+    {
+      start = false;
+    }
   }
 
   private void updatePos() {
@@ -55,6 +76,23 @@ class SoundTestCmp {
     }
     spd *= Const.REAL_TIME_STEP;
     myPos.x += spd;
+  }
+
+  private void change_color() {
+    c.set(myPos.x, myPos.y, 0, 1f);
+  }
+
+  private void change_radius(float d_time)
+  {
+    if (play_time <= .5f) //incrementing within half a second
+    {
+      radius += d_time * 0.1f; //amount of change in passed time
+    }
+    else //decrementing after
+    {
+      radius -= d_time * 0.1f;
+    }
+    play_time += d_time;
   }
 
   private void updateSound() {
