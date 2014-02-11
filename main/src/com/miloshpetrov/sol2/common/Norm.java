488066a6100a12e8e6297fe65e97cffb600c2291
miloshpetrov <ps6666@yandex.ru>
2014-02-11 22:23:18 +0000

    SolMath JavaDocs

--- main/src/com/miloshpetrov/sol2/common/Norm.java
+++ main/src/com/miloshpetrov/sol2/common/Norm.java
@@ -2,6 +2,10 @@ package com.miloshpetrov.sol2.common;
 
 import java.lang.annotation.Documented;
 
+/**
+ * When used for annotating methods, indicates that the returned float value is an angle and is normalized (-180 < a && a <= 180)
+ * When used for annotating params, indicates that the param angle must be normalized
+ */
 @Documented
 public @interface Norm {
 
