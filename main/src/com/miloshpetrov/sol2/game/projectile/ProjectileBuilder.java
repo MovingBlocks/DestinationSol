b3815ce90bb51f8a75fca9e6af8ca6cc37b46ebd
miloshpetrov <ps6666@yandex.ru>
2014-02-22 11:44:06 +0000

    removed projectiles.json

--- main/src/com/miloshpetrov/sol2/game/projectile/ProjectileBuilder.java
+++ main/src/com/miloshpetrov/sol2/game/projectile/ProjectileBuilder.java
@@ -1,28 +1,43 @@
 package com.miloshpetrov.sol2.game.projectile;
 
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.*;
+import com.miloshpetrov.sol2.Const;
+import com.miloshpetrov.sol2.common.Col;
 import com.miloshpetrov.sol2.common.SolMath;
-import com.miloshpetrov.sol2.game.*;
-import com.miloshpetrov.sol2.game.dra.Dra;
-import com.miloshpetrov.sol2.game.dra.DraLevel;
+import com.miloshpetrov.sol2.game.Fraction;
+import com.miloshpetrov.sol2.game.SolGame;
+import com.miloshpetrov.sol2.game.dra.*;
 import com.miloshpetrov.sol2.game.particle.LightSrc;
 import com.miloshpetrov.sol2.game.particle.ParticleSrc;
 
 import java.util.ArrayList;
 
 public class ProjectileBuilder {
-  private final PathLoader myPathLoader;
 
   public ProjectileBuilder() {
-    myPathLoader = new PathLoader("projectiles");
   }
 
   public Rocket buildRocket(SolGame game, Vector2 pos, float angle, Vector2 gunSpd, Fraction fraction, float dmg) {
     ArrayList<Dra> dras = new ArrayList<Dra>();
-    Body body = myPathLoader.getBodyAndSprite(game, "projectiles", "rocket", .15f, BodyDef.BodyType.DynamicBody, pos,
-      angle, dras, 1, DraLevel.PROJECTILES);
+    RectSprite s = new RectSprite(game.getTexMan().getTex("projectiles/rocket"), .15f, 0, 0, new Vector2(), DraLevel.PROJECTILES, 0, 0, Col.W);
+    dras.add(s);
+
+    BodyDef bd = new BodyDef();
+    bd.type = BodyDef.BodyType.DynamicBody;
+    bd.angle = angle * SolMath.degRad;
+    bd.angularDamping = 0;
+    bd.position.set(pos);
+    bd.linearDamping = 0;
+    Body body = game.getObjMan().getWorld().createBody(bd);
+    FixtureDef fd = new FixtureDef();
+    fd.density = 1;
+    fd.friction = Const.FRICTION;
+    fd.shape = new CircleShape();
+    fd.shape.setRadius(.05f);
+    body.createFixture(fd);
+    fd.shape.dispose();
+
     Vector2 spd = SolMath.fromAl(angle, Rocket.INITIAL_SPD_LEN);
     spd.add(gunSpd);
     body.setLinearVelocity(spd);
