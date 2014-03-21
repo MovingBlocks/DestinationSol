2bee047af34c27e463926877a34ecc978695eb64
miloshpetrov <ps6666@yandex.ru>
2014-03-21 07:14:44 +0000

    planet ready for new deco

--- main/src/com/miloshpetrov/sol2/game/dra/DraLevel.java
+++ main/src/com/miloshpetrov/sol2/game/dra/DraLevel.java
@@ -3,8 +3,7 @@ package com.miloshpetrov.sol2.game.dra;
 public enum DraLevel {
   NEBULAE(11), STARS(10),
   FAR_BG_3(2.5f), FAR_BG_2(2f), FAR_BG_1(1.5f),
-  ATM, JUNK, DECO, PART_BG_0, PART_BG_1, BIG_BODIES, BODIES, GUNS, PART_FG_0, PROJECTILES,
-  GROUND(.97f), CLOUDS(.94f);
+  ATM, JUNK, DECO, PART_BG_0, PART_BG_1, BIG_BODIES, BODIES, GUNS, PART_FG_0, PROJECTILES, GROUND, CLOUDS;
 
   public final float depth;
 
