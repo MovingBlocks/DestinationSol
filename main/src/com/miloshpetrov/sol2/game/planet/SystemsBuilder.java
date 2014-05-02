70a1caa563bdeb5339c7639a8046d139c021707f
miloshpetrov <ps6666@yandex.ru>
2014-05-02 18:18:02 +0000

    various improvements

--- main/src/com/miloshpetrov/sol2/game/planet/SystemsBuilder.java
+++ main/src/com/miloshpetrov/sol2/game/planet/SystemsBuilder.java
@@ -51,7 +51,7 @@ public class SystemsBuilder {
     ArrayList<Float> res = new ArrayList<Float>();
     boolean beltCreated = false;
     for (int i = 0; i < PLANET_COUNT; i++) {
-      boolean createBelt = !beltCreated && .3f * PLANET_COUNT < i && i < .7f * PLANET_COUNT && SolMath.test(.5f);
+      boolean createBelt = !beltCreated && 0 < i && i < .5f * PLANET_COUNT && SolMath.test(.5f);
       float gh;
       if (!createBelt) {
         gh = SolMath.rnd(.5f, 1) * Const.MAX_GROUND_HEIGHT;
