27ada10cd615af829fe3c596ca8dd1950576d404
miloshpetrov <ps6666@yandex.ru>
2014-05-04 10:21:31 +0000

    fractions doc updated

--- main/src/com/miloshpetrov/sol2/soundtest/SoundTestCmp.java
+++ main/src/com/miloshpetrov/sol2/soundtest/SoundTestCmp.java
@@ -1,69 +1,37 @@
 package com.miloshpetrov.sol2.soundtest;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.utils.TimeUtils;
-import com.miloshpetrov.sol2.*;
-import com.miloshpetrov.sol2.common.Col;
-import com.miloshpetrov.sol2.common.SolMath;
-import com.miloshpetrov.sol2.game.planet.*;
-import com.miloshpetrov.sol2.ui.DebugCollector;
-import com.miloshpetrov.sol2.ui.UiDrawer;
+import com.miloshpetrov.sol2.Const;
 
-import java.util.*;
+import java.util.ArrayList;
 
 class SoundTestCmp {
-  private static final float SPD = .5f;
-  private static final long SAMPLE_LENGTH = 4000;
-  private static long DEBUG_MUSIC_DIFF;
-
-  private final UiDrawer myUiDrawer;
-  private final Vector2 myPos;
-  private final Color myColor;
-  private final Map<String, List<SolSample>> mySamples;
-  private final List<SolSample> myCurrSamples;
-  private final Tile[][] myTileMap;
+  private final SpriteBatch mySpriteBatch;
+  private final ArrayList<TextureRegion> myTsDiv;
+  private final ArrayList<TextureRegion> myTsComb;
+  private final Texture myWt;
 
   private float myAccum;
-  private long myLastPlayTime;
+  private float myDivSum;
+  private float myCombSum;
 
   SoundTestCmp() {
-    TexMan texMan = new TexMan();
-    myUiDrawer = new UiDrawer(texMan);
-    myPos = new Vector2(.5f, .5f);
-    myColor = new Color(myPos.x, myPos.y, 0, 1f);
-    mySamples = new HashMap<String, List<SolSample>>();
-    myCurrSamples = new ArrayList<SolSample>();
-
-    FileHandle themeDir = SolFiles.readOnly("res/sounds/zones");
-    for (FileHandle fh : themeDir.list()) {
-      String name = fh.nameWithoutExtension();
-      String[] parts = name.split("_");
-      if (parts.length < 2) continue;
-      String cat = parts[0];
-      String idxStr = parts[1];
-      int idx;
-      try {
-        idx = Integer.parseInt(idxStr);
-      } catch (NumberFormatException e) {
-        continue;
-      }
-      List<SolSample> catSamples = mySamples.get(cat);
-      if (catSamples == null) {
-        catSamples = new ArrayList<SolSample>();
-        mySamples.put(cat, catSamples);
-      }
-      SolSample smp = new SolSample(cat, idx, Gdx.audio.newSound(fh));
-      catSamples.add(smp);
+    mySpriteBatch = new SpriteBatch();
+    myTsDiv = new ArrayList<TextureRegion>();
+    myTsComb = new ArrayList<TextureRegion>();
+    myWt = new Texture("imgSrcs/misc/whiteTex.png");
+    Texture full = new Texture("testImgs/testCombined.png");
+    for (int i = 0; i < 16; i++) {
+      Texture div = new Texture("testImgs/test_" + i + ".png");
+      TextureRegion divReg = new TextureRegion(div, 64, 64);
+      myTsDiv.add(divReg);
+      TextureRegion regComb = new TextureRegion(full, i * 64, 0, 64, 64);
+      myTsComb.add(regComb);
     }
-
-    PlanetConfig pc = new PlanetConfig("rocky", 0, 0, null, null, null, null, null, new PlanetTiles(texMan, "grounds/" + "rocky", null), null, null);
-    myTileMap = new GroundBuilder(pc, 40, 20).build();
   }
 
   public void render() {
@@ -77,117 +45,33 @@ class SoundTestCmp {
 
   private void draw() {
     Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-    myUiDrawer.begin();
-//    myUiDrawer.drawCircle(myPos, .05f, myColor);
-    drawTileMap();
-    DebugCollector.draw(myUiDrawer);
-    myUiDrawer.end();
+
+    mySpriteBatch.begin();
+    mySpriteBatch.draw(myWt, 0, 0);
+    long divided = dAndM(false);
+    mySpriteBatch.draw(myWt, 0, 0);
+    long combined = dAndM(true);
+    mySpriteBatch.draw(myWt, 0, 0);
+    mySpriteBatch.end();
+
+    myDivSum = divided;
+    myCombSum = combined;
   }
 
-  private void drawTileMap() {
-    float sz = .03f;
-    for (int col = 0; col < myTileMap.length; col++) {
-      Tile[] rows = myTileMap[col];
-      for (int row = 0; row < rows.length; row++) {
-        Tile t = rows[row];
-        if (t == null) continue;
-        drawDebugTile(t, sz, col, row);
-      }
+  private long dAndM(boolean combined) {
+    long s = TimeUtils.nanoTime();
+    ArrayList<TextureRegion> regs = combined ? myTsComb : myTsDiv;
+    for (int i = 0; i < regs.size(); i++) {
+      TextureRegion r = regs.get(i);
+      int y = i * 32;
+      if (combined) y += 64;
+      mySpriteBatch.draw(r, i * 32, y);
     }
-  }
-
-  private void drawDebugTile(Tile t, float sz, int col, int row) {
-    float x = (col + 1) * sz;
-    float y = (row + 1) * sz;
-//        myUiDrawer.draw(t.reg, sz*2, sz*2, 0, 0, x, y, 0, Col.W);
-    float szh = sz / 2;
-    if (t.from == SurfDir.FWD || t.from == SurfDir.UP) {
-      if (t.from == SurfDir.UP) drawQuarterTile(szh, x-szh, y-szh);
-      drawQuarterTile(szh, x-szh, y);
-    }
-    if (t.to == SurfDir.FWD || t.to == SurfDir.UP) {
-      if (t.to == SurfDir.UP) drawQuarterTile(szh, x, y-szh);
-      drawQuarterTile(szh, x, y);
-    }
-  }
-
-  private void drawQuarterTile(float szh, float x, float v) {
-    myUiDrawer.draw(myUiDrawer.whiteTex, szh, szh, 0, 0, x, v, 0, Col.W50);
+    long elapsed = TimeUtils.nanoTime() - s;
+    return elapsed;
   }
 
   private void update() {
-    DebugCollector.update();
-
-    updateMusic();
-    updatePos();
-    changeColor();
-  }
-
-  private void updateMusic() {
-    debug("delay:", DEBUG_MUSIC_DIFF);
-    for (SolSample s : myCurrSamples) {
-      debug(s.cat, ":", s.idx);
-    }
-
-    long now = TimeUtils.millis();
-    long diff = now - myLastPlayTime - SAMPLE_LENGTH;
-    if (diff < -Const.REAL_TIME_STEP / 2) return;
-    myLastPlayTime = now;
-    DEBUG_MUSIC_DIFF = diff;
-
-    myCurrSamples.clear();
-    for (List<SolSample> catSamples : mySamples.values()) {
-      SolSample sample = SolMath.elemRnd(catSamples);
-//      sample.s.play();
-      myCurrSamples.add(sample);
-    }
-    Collections.sort(myCurrSamples);
-  }
-
-  public void debug(Object ... objs) {
-    DebugCollector.debug(objs);
-  }
-
-  private void updatePos() {
-    float spd = 0;
-    if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && 0 < myPos.x) {
-      spd = -SPD;
-    } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && myPos.x < myUiDrawer.r) {
-      spd = SPD;
-    }
-    spd *= Const.REAL_TIME_STEP;
-    myPos.x += spd;
-
-    spd = 0;
-    if (Gdx.input.isKeyPressed(Input.Keys.UP) && 0 < myPos.y) {
-      spd = -SPD;
-    } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN) && myPos.y < 1) {
-      spd = SPD;
-    }
-    spd *= Const.REAL_TIME_STEP;
-    myPos.y += spd;
-  }
-
-  private void changeColor() {
-    myColor.set(myPos.x, myPos.y, 1, 1f);
-  }
-
-  private static class SolSample implements Comparable<SolSample> {
-    public final String cat;
-    public final int idx;
-    public final Sound s;
-
-    private SolSample(String cat, int idx, Sound s) {
-      this.cat = cat;
-      this.idx = idx;
-      this.s = s;
-    }
-
-    @Override
-    public int compareTo(SolSample o) {
-      int r = cat.compareTo(o.cat);
-      if (r != 0) return r;
-      return idx - o.idx;
-    }
+    System.out.println("Divided: " + myDivSum + ", combined: " + myCombSum);
   }
 }
