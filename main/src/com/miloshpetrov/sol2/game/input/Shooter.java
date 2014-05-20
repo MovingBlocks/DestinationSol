bfe05eddc9000da5f0255fa299e1d57e5e2fda35
miloshpetrov <ps6666@yandex.ru>
2014-05-20 16:28:22 +0000

    various planet improvements

--- main/src/com/miloshpetrov/sol2/game/input/Shooter.java
+++ main/src/com/miloshpetrov/sol2/game/input/Shooter.java
@@ -8,7 +8,7 @@ import com.miloshpetrov.sol2.game.ship.SolShip;
 public class Shooter {
 
   public static final float E_SPD_PERC = .6f; // 0 means that target speed is not considered, 1 means that it's fully considered
-  public static final float MIN_SHOOT_AAD = 1f;
+  public static final float MIN_SHOOT_AAD = 2f;
   private boolean myShoot;
   private boolean myShoot2;
   private boolean myRight;
