0bd6b66b67bdb498482662bf6039bc3a91afc792
miloshpetrov <ps6666@yandex.ru>
2014-05-22 12:55:22 +0000

    resources refactored

--- main/src/com/miloshpetrov/sol2/menu/MainScreen.java
+++ main/src/com/miloshpetrov/sol2/menu/MainScreen.java
@@ -41,7 +41,7 @@ public class MainScreen implements SolUiScreen {
     myExitCtrl.setDisplayName("Exit");
     myControls.add(myExitCtrl);
 
-    myTitleTex = texMan.getTex("misc/title", null);
+    myTitleTex = texMan.getTex("ui/title", null);
   }
 
   public List<SolUiControl> getControls() {
