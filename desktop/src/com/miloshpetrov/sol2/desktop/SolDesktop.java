507769c99b04d91305db307de55b521a2de3c6ab
Rasmus Praestholm <cervator@gmail.com>
2015-04-28 00:16:37 -0400

    Code formatting (no functional changes) and quick readme tweak

--- desktop/src/com/miloshpetrov/sol2/desktop/SolDesktop.java
+++ desktop/src/com/miloshpetrov/sol2/desktop/SolDesktop.java
@@ -15,54 +15,58 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SolDesktop {
-  public static void main(String[] argv) {
-    if (false) {
-      new LwjglApplication(new SoundTestListener(), "sound test", 800, 600, false);
-      return;
-    }
-
-    boolean devBuild = java.nio.file.Files.exists(Paths.get("devBuild"));
-    if (devBuild) DebugOptions.DEV_ROOT_PATH = "main/"; // supposing that solWin is in the same directory where trunk is.
-    MyReader reader = new MyReader();
-    DebugOptions.read(reader);
-
-    LwjglApplicationConfiguration c = new LwjglApplicationConfiguration();
-    if (DebugOptions.EMULATE_MOBILE) {
-      c.width = 640;
-      c.height = 480;
-      c.fullscreen = false;
-    } else {
-      GameOptions d = new GameOptions(false, reader);
-      c.width = d.x;
-      c.height = d.y;
-      c.fullscreen = d.fullscreen;
-    }
-
-    c.title = "Destination Sol";
-    if (DebugOptions.DEV_ROOT_PATH == null) {
-      c.addIcon("res/icon.png", Files.FileType.Internal);
-    } else {
-      c.addIcon(DebugOptions.DEV_ROOT_PATH + "res/icon.png", Files.FileType.Absolute);
-    }
-
-    new LwjglApplication(new SolAppListener(), c);
-  }
-
-  private static class MyReader implements SolFileReader {
-    @Override
-    public List<String> read(String fileName) {
-      if (DebugOptions.DEV_ROOT_PATH != null) fileName = DebugOptions.DEV_ROOT_PATH + fileName;
-      ArrayList<String> lines = new ArrayList<String>();
-      try {
-        BufferedReader br = new BufferedReader(new FileReader(fileName));
-        String line = "";
-        while ((line = br.readLine()) != null) {
-          lines.add(line);
+    public static void main(String[] argv) {
+        if (false) {
+            new LwjglApplication(new SoundTestListener(), "sound test", 800, 600, false);
+            return;
         }
-        br.close();
-      } catch (IOException ignore) {
-      }
-      return lines;
+
+        boolean devBuild = java.nio.file.Files.exists(Paths.get("devBuild"));
+        if (devBuild) {
+            DebugOptions.DEV_ROOT_PATH = "main/"; // Lets the game run from source without a tweaked working directory
+        }
+        MyReader reader = new MyReader();
+        DebugOptions.read(reader);
+
+        LwjglApplicationConfiguration c = new LwjglApplicationConfiguration();
+        if (DebugOptions.EMULATE_MOBILE) {
+            c.width = 640;
+            c.height = 480;
+            c.fullscreen = false;
+        } else {
+            GameOptions d = new GameOptions(false, reader);
+            c.width = d.x;
+            c.height = d.y;
+            c.fullscreen = d.fullscreen;
+        }
+
+        c.title = "Destination Sol";
+        if (DebugOptions.DEV_ROOT_PATH == null) {
+            c.addIcon("res/icon.png", Files.FileType.Internal);
+        } else {
+            c.addIcon(DebugOptions.DEV_ROOT_PATH + "res/icon.png", Files.FileType.Absolute);
+        }
+
+        new LwjglApplication(new SolAppListener(), c);
     }
-  }
+
+    private static class MyReader implements SolFileReader {
+        @Override
+        public List<String> read(String fileName) {
+            if (DebugOptions.DEV_ROOT_PATH != null) {
+                fileName = DebugOptions.DEV_ROOT_PATH + fileName;
+            }
+            ArrayList<String> lines = new ArrayList<String>();
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(fileName));
+                String line = "";
+                while ((line = br.readLine()) != null) {
+                    lines.add(line);
+                }
+                br.close();
+            } catch (IOException ignore) {
+            }
+            return lines;
+        }
+    }
 }
