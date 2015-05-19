fa9fcaa6d6f5c6ff373483f592b1dec03cfddf42
SimonC4 <simon.crutchley@gmail.com>
2015-05-19 22:39:58 +1200

    In order to compile on OS X using Java 8 I had to update the lwjgl libraries. Can now compile and run on OS X with Java 8.
    Updated the lwjgl libraries and fixed deprecated issues including:
    * Updating to OpenGL ES 2.0 as 1.0 is no longer supported. http://www.badlogicgames.com/wordpress/?p=3311
    * Vector2 deprecations
    * Box2D moved to an extension: http://www.badlogicgames.com/wordpress/?p=3404
    * Bitmap Font refactoring: http://www.badlogicgames.com/wordpress/?p=3658
    
    Known issues:
    1. The mouse pointer
    2. Android will need updated libraries.

--- main/src/com/miloshpetrov/sol2/soundtest/SoundTestCmp.java
+++ main/src/com/miloshpetrov/sol2/soundtest/SoundTestCmp.java
@@ -1,7 +1,7 @@
 package com.miloshpetrov.sol2.soundtest;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.utils.TimeUtils;
@@ -44,7 +44,7 @@ class SoundTestCmp {
   }
 
   private void draw() {
-    Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
     mySpriteBatch.begin();
     mySpriteBatch.draw(myWt, 0, 0);
