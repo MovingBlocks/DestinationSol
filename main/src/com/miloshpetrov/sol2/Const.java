8b70f6cec64fc2673ede4f15e4939854b4dbaa4f
Rasmus Praestholm <cervator@gmail.com>
2015-07-09 22:03:03 -0400

    Version bump to 1.2.0 - next minor release

--- main/src/com/miloshpetrov/sol2/Const.java
+++ main/src/com/miloshpetrov/sol2/Const.java
@@ -10,7 +10,7 @@ public class Const {
   public static final float CHUNK_SIZE = 20f;
   public static final int ITEM_GROUPS_PER_PAGE = 8;
   public final static float PLANET_GAP = 8f;
-  public static final String VERSION = "1.1.1";
+  public static final String VERSION = "1.2.0";
   public static final float FRICTION = .5f;
   public static final float IMPULSE_TO_COLL_VOL = 2f;
 
