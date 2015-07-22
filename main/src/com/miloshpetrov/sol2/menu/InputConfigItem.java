2319e2fed53df64112f24e1df50a8999b4c80051
SimonC4 <simon.crutchley@gmail.com>
2015-07-22 21:50:57 +1200

    Display the controller inputs and reset to defaults.

--- main/src/com/miloshpetrov/sol2/menu/InputConfigItem.java
+++ main/src/com/miloshpetrov/sol2/menu/InputConfigItem.java
@@ -8,6 +8,8 @@ package com.miloshpetrov.sol2.menu;
 public class InputConfigItem {
     private String displayName;
     private String inputKey;
+    private boolean isAxis = true;
+    private int controllerInput = -1;
 
     /**
      * Get the key mappings to display on the input mapping screen.
@@ -17,6 +19,20 @@ public class InputConfigItem {
     InputConfigItem(String name, String key) {
         setDisplayName(name);
         setInputKey(key);
+    }
+
+    /**
+     * Get the key mappings to display on the input mapping screen.
+     * @param name The input name to display on the screen
+     * @param key The key the input is mapped to
+     * @param isAxis true if the input is an axis, false if is a button
+     * @param key The button or axis the input is mapped to
+     */
+    InputConfigItem(String name, String key, boolean isAxis, int controllerInput) {
+        setDisplayName(name);
+        setInputKey(key);
+        setIsAxis(isAxis);
+        setControllerInput(controllerInput);
     }
 
     /**
@@ -49,5 +65,37 @@ public class InputConfigItem {
      */
     public void setInputKey(String key) {
         inputKey = key;
+    }
+
+    /**
+     * Get the controller input type
+     * @return boolean true if the input is an axis, false if is a button
+     */
+    public boolean isAxis() {
+        return isAxis;
+    }
+
+    /**
+     * Set the controller input type
+     * @param isAxis true if the input is an axis, false if is a button
+     */
+    public void setIsAxis(boolean isAxis) {
+        this.isAxis = isAxis;
+    }
+
+    /**
+     * Get the controller input
+     * @return int The button or axis the input is mapped to
+     */
+    public int getControllerInput() {
+        return controllerInput;
+    }
+
+    /**
+     * Set the controller input type
+     * @param controllerInput The button or axis the input is mapped to
+     */
+    public void setControllerInput(int controllerInput) {
+        this.controllerInput = controllerInput;
     }
 }
