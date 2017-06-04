0447fa119a7d324f41d3488446407ec0d2f7a64e
vampcat <areshhitman@gmail.com>
2017-06-04 09:44:12 +0530

    Upgraded Module Discovery Log

--- main/src/org/destinationsol/ModuleManager.java
+++ main/src/org/destinationsol/ModuleManager.java
@@ -16,10 +16,18 @@
 package org.destinationsol;
 
 import com.google.common.collect.Sets;
+import org.destinationsol.assets.AssetHelper;
 import org.destinationsol.assets.Assets;
+import org.destinationsol.assets.atlas.Atlas;
+import org.destinationsol.assets.audio.OggMusic;
+import org.destinationsol.assets.audio.OggSound;
+import org.destinationsol.assets.emitters.Emitter;
+import org.destinationsol.assets.json.Json;
+import org.destinationsol.assets.textures.DSTexture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.assets.Asset;
+import org.terasology.assets.ResourceUrn;
 import org.terasology.module.ClasspathModule;
 import org.terasology.module.Module;
 import org.terasology.module.ModuleEnvironment;
@@ -91,8 +99,110 @@ public class ModuleManager {
     }
 
     public void printAvailableModules() {
+        AssetHelper assetHelper = Assets.getAssetHelper();
+        Set<ResourceUrn> jsonList = assetHelper.list(Json.class);
+        Set<ResourceUrn> emitterList = assetHelper.list(Emitter.class);
+        Set<ResourceUrn> atlasList = assetHelper.list(Atlas.class);
+        Set<ResourceUrn> soundList = assetHelper.list(OggSound.class);
+        Set<ResourceUrn> musicList = assetHelper.list(OggMusic.class);
+        Set<ResourceUrn> textureList = assetHelper.list(DSTexture.class);
+
         for (Module module : registry) {
+            String moduleName = module.getId().toString();
+
             logger.info("Module Discovered: {}", module.toString());
+
+            int armors = 0;
+            int abilityCharges = 0;
+            int clips = 0;
+            int engines = 0;
+            int shields = 0;
+            int jsonOthers = 0;
+            int emitters = 0;
+            int atlas = 0;
+            int sounds = 0;
+            int music = 0;
+            int textures = 0;
+
+            for (ResourceUrn assetUrn : jsonList) {
+                String assetName = assetUrn.toString();
+
+                if (assetName.startsWith(moduleName + ":")) {
+                    if (assetName.endsWith("Armor")) {
+                        armors++;
+                    } else if (assetName.endsWith("AbilityCharge")) {
+                        abilityCharges++;
+                    } else if (assetName.endsWith("Clip")) {
+                        clips++;
+                    } else if (assetName.endsWith("Engine")) {
+                        engines++;
+                    } else if (assetName.endsWith("Shield")) {
+                        shields++;
+                    } else {
+                        jsonOthers++;
+                    }
+                }
+            }
+
+            for (ResourceUrn assetUrn : emitterList) {
+                String assetName = assetUrn.toString();
+
+                if (assetName.startsWith(moduleName + ":")) {
+                    emitters++;
+                }
+            }
+
+            for (ResourceUrn assetUrn : atlasList) {
+                String assetName = assetUrn.toString();
+
+                if (assetName.startsWith(moduleName + ":")) {
+                    atlas++;
+                }
+            }
+
+            for (ResourceUrn assetUrn : soundList) {
+                String assetName = assetUrn.toString();
+
+                if (assetName.startsWith(moduleName + ":")) {
+                    sounds++;
+                }
+            }
+
+            for (ResourceUrn assetUrn : musicList) {
+                String assetName = assetUrn.toString();
+
+                if (assetName.startsWith(moduleName + ":")) {
+                    music++;
+                }
+            }
+
+            for (ResourceUrn assetUrn : textureList) {
+                String assetName = assetUrn.toString();
+
+                if (assetName.startsWith(moduleName + ":")) {
+                    textures++;
+                }
+            }
+
+            logger.info("\t-Items:");
+            logger.info("\t\t-Armors: {}", armors);
+            logger.info("\t\t-AbilityCharges: {}", abilityCharges);
+            logger.info("\t\t-Clips: {}", clips);
+            logger.info("\t\t-Engines: {}", engines);
+            logger.info("\t\t-Shields: {}", shields);
+            logger.info("\t\t-Others: {}", jsonOthers);
+
+            logger.info("\t-Atlas: {}", atlas);
+
+            logger.info("\t-Emitters: {}", emitters);
+
+            logger.info("\t-Sounds: {}", sounds);
+
+            logger.info("\t-Music: {}", music);
+
+            logger.info("\t-Textures: {}", textures);
+
+            logger.info("");
         }
     }
 }
