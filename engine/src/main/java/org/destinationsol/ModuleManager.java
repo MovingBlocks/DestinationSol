372560b74c8c9b01354ac2a58e739c6a0277a803
TheGhostInTheMachine <nitrogen1818@gmail.com>
2017-12-21 23:26:41 -0500

    enable module

--- engine/src/main/java/org/destinationsol/ModuleManager.java
+++ engine/src/main/java/org/destinationsol/ModuleManager.java
@@ -96,6 +96,7 @@ public class ModuleManager {
         requiredModules.add(registry.getLatestModuleVersion(new Name("organic")));
         requiredModules.add(registry.getLatestModuleVersion(new Name("tribe")));
         requiredModules.add(registry.getLatestModuleVersion(new Name("deepspace")));
+        requiredModules.add(registry.getLatestModuleVersion(new Name("formic")));
 
         loadEnvironment(requiredModules);
     }
