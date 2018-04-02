5956b1df7772958c1e60a1e551a539f3d143b744
oniatus <oniatus@googlemail.com>
2018-04-02 13:09:31 +0200

    Move createPlayer to method object

--- engine/src/main/java/org/destinationsol/game/SolGame.java
+++ engine/src/main/java/org/destinationsol/game/SolGame.java
@@ -30,17 +30,11 @@ import org.destinationsol.game.chunk.ChunkManager;
 import org.destinationsol.game.drawables.DrawableDebugger;
 import org.destinationsol.game.drawables.DrawableManager;
 import org.destinationsol.game.farBg.FarBackgroundManagerOld;
-import org.destinationsol.game.input.AiPilot;
-import org.destinationsol.game.input.BeaconDestProvider;
-import org.destinationsol.game.input.Pilot;
-import org.destinationsol.game.input.UiControlledPilot;
-import org.destinationsol.game.item.Gun;
 import org.destinationsol.game.item.ItemContainer;
 import org.destinationsol.game.item.ItemManager;
 import org.destinationsol.game.item.LootBuilder;
 import org.destinationsol.game.item.MercItem;
 import org.destinationsol.game.item.SolItem;
-import org.destinationsol.game.item.TradeConfig;
 import org.destinationsol.game.particle.EffectTypes;
 import org.destinationsol.game.particle.PartMan;
 import org.destinationsol.game.particle.SpecialEffects;
@@ -166,7 +160,7 @@ public class SolGame {
         if (!respawnState.isPlayerRespawned()) {
             game.getGalaxyFiller().fill(game, game.getHullConfigs(), game.getItemMan());
         }
-        hero = createPlayer(shipConfig,
+        hero = new PlayerCreator().createPlayer(shipConfig,
                 shouldSpawnOnGalaxySpawnPosition,
                 respawnState,
                 this,
@@ -180,67 +174,6 @@ public class SolGame {
         } else {
             return SaveManager.readShip(game.getHullConfigs(), game.getItemMan(), game);
         }
-    }
-
-    // uh, this needs refactoring
-    private Hero createPlayer(ShipConfig shipConfig, boolean shouldSpawnOnGalaxySpawnPosition, RespawnState respawnState, SolGame game, boolean isMouseControl, boolean isNewShip) {
-        // If we continue a game, we should spawn from the same position
-        Vector2 position;
-        if (shouldSpawnOnGalaxySpawnPosition) {
-            position = game.getGalaxyFiller().getPlayerSpawnPos(game);
-        } else {
-            position = shipConfig.spawnPos;
-        }
-        game.getCam().setPos(position);
-
-        Pilot pilot;
-        if (isMouseControl) {
-            game.getBeaconHandler().init(game, position);
-            pilot = new AiPilot(new BeaconDestProvider(), true, Faction.LAANI, false, "you", Const.AI_DET_DIST);
-        } else {
-            pilot = new UiControlledPilot(game.getScreens().mainScreen);
-        }
-
-        float money = respawnState.getRespawnMoney() != 0 ? respawnState.getRespawnMoney() : game.getTutMan() != null ? 200 : shipConfig.money;
-
-        HullConfig hull = respawnState.getRespawnHull() != null ? respawnState.getRespawnHull() : shipConfig.hull;
-
-        String itemsStr = !respawnState.getRespawnItems().isEmpty() ? "" : shipConfig.items;
-
-        boolean giveAmmo = isNewShip && respawnState.getRespawnItems().isEmpty();
-        Hero hero = new Hero(game.getShipBuilder().buildNewFar(game, new Vector2(position), null, 0, 0, pilot, itemsStr, hull, null, true, money, new TradeConfig(), giveAmmo).toObject(game));
-        ItemContainer itemContainer = hero.getItemContainer();
-        if (!respawnState.getRespawnItems().isEmpty()) {
-            for (SolItem item : respawnState.getRespawnItems()) {
-                itemContainer.add(item);
-                // Ensure that previously equipped items stay equipped
-                if (item.isEquipped() > 0) {
-                    if (item instanceof Gun) {
-                        hero.maybeEquip(game, item, item.isEquipped() == 2, true);
-                    } else {
-                        hero.maybeEquip(game, item, true);
-                    }
-                }
-            }
-        } else if (game.getTutMan() != null) {
-            for (int i = 0; i < 50; i++) {
-                if (itemContainer.groupCount() > 1.5f * Const.ITEM_GROUPS_PER_PAGE) {
-                    break;
-                }
-                SolItem it = game.getItemMan().random();
-                if (!(it instanceof Gun) && it.getIcon(game) != null && itemContainer.canAdd(it)) {
-                    itemContainer.add(it.copy());
-                }
-            }
-        }
-        itemContainer.markAllAsSeen();
-
-        // Don't change equipped items across load/respawn
-        //AiPilot.reEquip(this, myHero);
-
-        game.getObjectManager().addObjDelayed(hero.getShip());
-        game.getObjectManager().resetDelays();
-        return hero;
     }
 
     private void createAndSpawnMercenariesFromSave() {
