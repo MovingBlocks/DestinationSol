b7955ff1155179b95d2404a1c200488b2c24fa0c
Adrijaned <adrijaned@gmail.com>
2018-04-16 14:55:47 +0200

    SolDesktop - added copyright

--- desktop/src/main/java/org/destinationsol/desktop/SolDesktop.java
+++ desktop/src/main/java/org/destinationsol/desktop/SolDesktop.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.destinationsol.desktop;
 
 import com.badlogic.gdx.Files;
@@ -11,7 +26,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.crashreporter.CrashReporter;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
