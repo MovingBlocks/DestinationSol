f792a36bcffe1bacecfbb188718251d495d9dbc6
Adrijaned <adrijaned@gmail.com>
2018-04-28 16:32:09 +0200

    Context javadoc now makes sense for DS

--- engine/src/main/java/org/destinationsol/game/context/Context.java
+++ engine/src/main/java/org/destinationsol/game/context/Context.java
@@ -18,13 +18,8 @@ package org.destinationsol.game.context;
 /**
  * Provides classes with the utility objects that belong to the context they are running in.
  *
- * Use dependency injection or this interface to get at the objects you want to use.
- *
- *
  * From this class there can be multiple instances. For example we have the option of letting a client and server run
  * concurrently in one VM, by letting them work with two separate context objects.
- *
- * This class is intended to replace the CoreRegistry and other static means to get utility objects.
  *
  * Contexts must be thread safe!
  */
