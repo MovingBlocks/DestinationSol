c4db5fdea1ca2fbd184f93be4f691d69b6e367f1
Adrijaned <adrijaned@gmail.com>
2018-05-18 17:18:42 +0200

    refactored musicmanager used globally, javadoc

--- engine/src/main/java/org/destinationsol/assets/audio/OggMusicManager.java
+++ engine/src/main/java/org/destinationsol/assets/audio/OggMusicManager.java
@@ -34,29 +34,40 @@ public class OggMusicManager {
     public static final String MENU_MUSIC_SET = "menu";
     public static final String GAME_MUSIC_SET = "game";
     private final Map<String, List<Music>> musicMap;
-    private final Music menuMusic;
-    private final List<Music> gameMusic;
-    private Music currentlyPlaying = null;
+    private Music currentlyPlaying;
 
+    /**
+     * Registers engine music.
+     * TODO Make music registerable from modules, and then register this music through the new way.
+     */
     public OggMusicManager() {
         musicMap = new HashMap<>();
-        menuMusic = Assets.getMusic("engine:dreadnaught").getMusic();
-        menuMusic.setLooping(true);
         registerMusic(MENU_MUSIC_SET, "engine:dreadnaught");
-
-        gameMusic = new ArrayList<>();
-        gameMusic.add(Assets.getMusic("engine:cimmerianDawn").getMusic());
-        gameMusic.add(Assets.getMusic("engine:intoTheDark").getMusic());
-        gameMusic.add(Assets.getMusic("engine:spaceTheatre").getMusic());
         registerMusic(GAME_MUSIC_SET, "engine:cimmerianDawn");
         registerMusic(GAME_MUSIC_SET, "engine:intoTheDark");
         registerMusic(GAME_MUSIC_SET, "engine:spaceTheatre");
     }
 
+    /**
+     *Registers a music track into a music set.
+     *
+     * Once registered, the track may then be played by {@link #playMusic(String, GameOptions)}. The track will be
+     * played even if the {@code musicSet} set is already playing.
+     * @param musicSet Name of the set to register to
+     * @param music Fully qualified name of the music to register (eg. {@code "engine:dreadnaught"})
+     */
     public void registerMusic(String musicSet, String music) {
         registerMusic(musicSet, Assets.getMusic(music).getMusic());
     }
 
+    /**
+     * Registers a music track into a music set.
+     *
+     * Once registered, the track may then be played by {@link #playMusic(String, GameOptions)}. The track will be
+     * played even if the {@code musicSet} set is already playing.
+     * @param musicSet Name of the set to register to
+     * @param music Music to register
+     */
     public void registerMusic(String musicSet, Music music) {
         if (!musicMap.containsKey(musicSet)) {
             musicMap.put(musicSet, new ArrayList<>());
@@ -65,8 +76,21 @@ public class OggMusicManager {
 
     }
 
+    /**
+     * Sets a music set to play music from.
+     *
+     * When end of each of tracks in the music set is reached, a neext song from the set is then played. When there are
+     * no more tracks in a set, the first one is played again. When an invalid music set name is passed in, the music
+     * just stops playing.
+     *
+     * @param musicSet Name of the set to play.
+     * @param options GameOptions with volume for the music to have.
+     */
     public void playMusic(final String musicSet, final GameOptions options) {
         stopMusic();
+        if (musicMap.get(musicSet).isEmpty()) {
+            return;
+        }
         int index = 0;
         if (currentlyPlaying != null && musicMap.get(musicSet).contains(currentlyPlaying)) {
             // skip the track
@@ -81,32 +105,32 @@ public class OggMusicManager {
     }
 
     /**
-     * Start playing the music menu from the beginning of the track. The menu music loops continuously.
+     * Plays a music track and sets it as current.
+     *
+     * @param music Music track to play
+     * @param options Options with volume for the track
      */
-    public void playMenuMusic(GameOptions options) {
-        playMusic(MENU_MUSIC_SET, options);
-    }
-
-    public void playGameMusic(final GameOptions options) {
-        playMusic(GAME_MUSIC_SET, options);
-    }
-
-    public void playMusicTrack(Music music, GameOptions options) {
+    private void playMusicTrack(Music music, GameOptions options) {
         currentlyPlaying = music;
         currentlyPlaying.setVolume(options.musicVolumeMultiplier);
         currentlyPlaying.play();
     }
 
     /**
-     * Stop playing all music.
+     * Stop playing current music track.
      */
-    public void stopMusic() {
+    private void stopMusic() {
         if (currentlyPlaying != null) {
             currentlyPlaying.stop();
         }
     }
 
-    public void resetVolume(GameOptions options) {
+    /**
+     * Changes volume of currently playing track.
+     *
+     * @param options GameOptions containing the requested volume.
+     */
+    public void changeVolume(GameOptions options) {
         currentlyPlaying.setVolume(options.musicVolumeMultiplier);
     }
 }
