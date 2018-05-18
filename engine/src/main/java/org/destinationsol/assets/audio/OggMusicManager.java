dc292d6f1e065d241c156285ec62afc07888316f
Adrijaned <adrijaned@gmail.com>
2018-05-18 16:35:35 +0200

    refactored musicmanager

--- engine/src/main/java/org/destinationsol/assets/audio/OggMusicManager.java
+++ engine/src/main/java/org/destinationsol/assets/audio/OggMusicManager.java
@@ -20,61 +20,78 @@ import org.destinationsol.GameOptions;
 import org.destinationsol.assets.Assets;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Class responsible for playing all music throughout the game.
- *
+ * <p>
  * This class does not rely on external updates; once a music set is set to be played, it will play, even looping,
- * until another is chosen.
+ * until another is chosen. By default, music does not play concurrently.
  */
 public class OggMusicManager {
+    public static final String MENU_MUSIC_SET = "menu";
+    public static final String GAME_MUSIC_SET = "game";
+    private final Map<String, List<Music>> musicMap;
     private final Music menuMusic;
     private final List<Music> gameMusic;
     private Music currentlyPlaying = null;
 
     public OggMusicManager() {
+        musicMap = new HashMap<>();
         menuMusic = Assets.getMusic("engine:dreadnaught").getMusic();
         menuMusic.setLooping(true);
+        registerMusic(MENU_MUSIC_SET, "engine:dreadnaught");
 
         gameMusic = new ArrayList<>();
         gameMusic.add(Assets.getMusic("engine:cimmerianDawn").getMusic());
         gameMusic.add(Assets.getMusic("engine:intoTheDark").getMusic());
         gameMusic.add(Assets.getMusic("engine:spaceTheatre").getMusic());
+        registerMusic(GAME_MUSIC_SET, "engine:cimmerianDawn");
+        registerMusic(GAME_MUSIC_SET, "engine:intoTheDark");
+        registerMusic(GAME_MUSIC_SET, "engine:spaceTheatre");
+    }
+
+    public void registerMusic(String musicSet, String music) {
+        registerMusic(musicSet, Assets.getMusic(music).getMusic());
+    }
+
+    public void registerMusic(String musicSet, Music music) {
+        if (!musicMap.containsKey(musicSet)) {
+            musicMap.put(musicSet, new ArrayList<>());
+        }
+        musicMap.get(musicSet).add(music);
+
+    }
+
+    public void playMusic(final String musicSet, final GameOptions options) {
+        stopMusic();
+        int index = 0;
+        if (currentlyPlaying != null && musicMap.get(musicSet).contains(currentlyPlaying)) {
+            // skip the track
+            index = musicMap.get(musicSet).indexOf(currentlyPlaying);
+            if (++index + 1 > musicMap.get(musicSet).size()) { // next track, plus one because indexing is from 0
+                index = 0;
+            }
+        }
+        final Music music = musicMap.get(musicSet).get(index);
+        music.setOnCompletionListener(a -> playMusic(musicSet, options));
+        playMusicTrack(music, options);
     }
 
     /**
      * Start playing the music menu from the beginning of the track. The menu music loops continuously.
      */
     public void playMenuMusic(GameOptions options) {
-        if (currentlyPlaying != null) {
-            if (currentlyPlaying != menuMusic || !currentlyPlaying.isPlaying()) {
-                stopMusic();
-                playMusic(menuMusic, options);
-            }
-        } else {
-            stopMusic();
-            playMusic(menuMusic, options);
-        }
+        playMusic(MENU_MUSIC_SET, options);
     }
 
     public void playGameMusic(final GameOptions options) {
-        stopMusic();
-        if (currentlyPlaying != null && gameMusic.contains(currentlyPlaying)) {
-            int index = gameMusic.indexOf(currentlyPlaying) + 1;
-            if (gameMusic.size() - 1 >= index) {
-                playMusic(gameMusic.get(index), options);
-                currentlyPlaying.setOnCompletionListener(music -> playGameMusic(options));
-
-            } else {
-                playMusic(gameMusic.get(0), options);
-            }
-        } else {
-            playMusic(gameMusic.get(0), options);
-        }
+        playMusic(GAME_MUSIC_SET, options);
     }
 
-    public void playMusic(Music music, GameOptions options) {
+    public void playMusicTrack(Music music, GameOptions options) {
         currentlyPlaying = music;
         currentlyPlaying.setVolume(options.musicVolumeMultiplier);
         currentlyPlaying.play();
