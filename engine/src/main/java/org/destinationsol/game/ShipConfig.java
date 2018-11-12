9259bf74f40e0c57195fea01fb5bd9b143d644f0
Adrijaned <adrijaned@gmail.com>
2018-11-12 16:12:06 +0100

    tiny schema fixes && improvements, some config schemas

--- engine/src/main/java/org/destinationsol/game/ShipConfig.java
+++ engine/src/main/java/org/destinationsol/game/ShipConfig.java
@@ -25,6 +25,7 @@ import org.destinationsol.assets.json.Json;
 import org.destinationsol.files.HullConfigManager;
 import org.destinationsol.game.item.ItemManager;
 import org.destinationsol.game.ship.hulls.HullConfig;
+import org.everit.json.schema.loader.SchemaLoader;
 import org.terasology.assets.ResourceUrn;
 
 import java.util.ArrayList;
@@ -111,6 +112,7 @@ public class ShipConfig {
         if (rootNode == null) {
             return null;
         }
+        SchemaLoader.builder().draftV7Support();
 
         String hullName = rootNode.getString("hull");
         HullConfig hull = hullConfigs.getConfig(hullName);
