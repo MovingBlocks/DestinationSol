4e87cf7f59ea7f7e7fff56e669c3ad33db440814
Nathan Schnitzer <120NSchnitzer@frhsd.com>
2018-11-12 13:22:41 -0500

    Changed Spacing from tabs to spaces

--- engine/src/main/java/org/destinationsol/game/PlayerSpawnConfig.java
+++ engine/src/main/java/org/destinationsol/game/PlayerSpawnConfig.java
@@ -23,60 +23,60 @@ import org.destinationsol.files.HullConfigManager;
 import org.destinationsol.game.item.ItemManager;
 
 public class PlayerSpawnConfig {
-	final ShipConfig mainStation;
-	final ShipConfig godShipConfig;
-	final ShipConfig shipConfig;
+    final ShipConfig mainStation;
+    final ShipConfig godShipConfig;
+    final ShipConfig shipConfig;
 
-	PlayerSpawnConfig(ShipConfig shipConfig, ShipConfig mainStation, ShipConfig godShipConfig) {
-		this.shipConfig = shipConfig;
-		this.mainStation = mainStation;
-		this.godShipConfig = godShipConfig;
-	}
+    PlayerSpawnConfig(ShipConfig shipConfig, ShipConfig mainStation, ShipConfig godShipConfig) {
+        this.shipConfig = shipConfig;
+        this.mainStation = mainStation;
+        this.godShipConfig = godShipConfig;
+    }
 
-	public static PlayerSpawnConfig load(HullConfigManager hullConfigs, ItemManager itemManager) {
-		Json json = Assets.getJson("engine:playerSpawnConfig");
-		JsonValue rootNode = json.getJsonValue();
-		ShipConfig shipConfig, mainStation, godShipConfig;
+    public static PlayerSpawnConfig load(HullConfigManager hullConfigs, ItemManager itemManager) {
+        Json json = Assets.getJson("engine:playerSpawnConfig");
+        JsonValue rootNode = json.getJsonValue();
+        ShipConfig shipConfig, mainStation, godShipConfig;
 
-		if (rootNode.get("players") != null) {
-			throw new SolException("Please rename 'players' to 'player' in the JSON");
-		}
-		else if (rootNode.get("player") == null) {
-			throw new SolException("There is no player in the JSON of " + rootNode);
-		}
-		else if (rootNode.get("player") != null) {
-			JsonValue playerNode = rootNode.get("player");
-			if (playerNode.get("ship") == null){
-				throw new SolException("The player, " + playerNode + " ,does not have a ship!");
-			}
-			else if (playerNode.get("ships") != null) {
-				throw new SolException("Please rename 'ships' to 'ship' in the JSON");
-			}
-			else {
-				shipConfig = ShipConfig.load(hullConfigs, playerNode.get("ship"), itemManager);
-				if (playerNode.get("godModeShip") == null) {
-					throw new SolException("The player, " + playerNode + " ,does not have a godModeShip!");
-				}
-				else if (playerNode.get("godModeShips" != null)) {
-					throw new SolException("Please rename 'godModeShips' to 'godModeShip' in the JSON");
-				}
-				else {
-					godShipConfig = ShipConfig.load(hullConfigs, playerNode.get("godModeShip"), itemManager);
-					if (rootNode.get("mainStation") == null) {
-						throw new SolException("rootNode, " + rootNode + " ,does not have a main station");
-					}
-					else if (rootNode.get("mainStations") != null) {
-						throw new SolException("Please rename 'mainStations to 'mainStation' in the JSON");
-					}
-					else {
-						mainStation = ShipConfig.load(hullConfigs, rootNode.get("mainStation"), itemManager);
-					}
-				}
-			}  
+        if (rootNode.get("players") != null) {
+            throw new SolException("Please rename 'players' to 'player' in the JSON");
+        }
+        else if (rootNode.get("player") == null) {
+            throw new SolException("There is no player in the JSON of " + rootNode);
+        }
+        else {
+            JsonValue playerNode = rootNode.get("player");
+            if (playerNode.get("ship") == null){
+                throw new SolException("The player, " + playerNode + " ,does not have a ship!");
+            }
+            else if (playerNode.get("ships") != null) {
+                throw new SolException("Please rename 'ships' to 'ship' in the JSON");
+            }
+            else {
+                shipConfig = ShipConfig.load(hullConfigs, playerNode.get("ship"), itemManager);
+                if (playerNode.get("godModeShip") == null) {
+                    throw new SolException("The player, " + playerNode + " ,does not have a godModeShip!");
+                }
+                else if (playerNode.get("godModeShips") != null) {
+                    throw new SolException("Please rename 'godModeShips' to 'godModeShip' in the JSON");
+                }
+                else {
+                    godShipConfig = ShipConfig.load(hullConfigs, playerNode.get("godModeShip"), itemManager);
+                    if (rootNode.get("mainStation") == null) {
+                        throw new SolException("rootNode, " + rootNode + " ,does not have a main station");
+                    }
+                    else if (rootNode.get("mainStations") != null) {
+                        throw new SolException("Please rename 'mainStations to 'mainStation' in the JSON");
+                    }
+                    else {
+                        mainStation = ShipConfig.load(hullConfigs, rootNode.get("mainStation"), itemManager);
+                    }
+                }
+            }  
 
-			json.dispose();
+            json.dispose();
 
-			return new PlayerSpawnConfig(shipConfig, mainStation, godShipConfig);
-		}
-	}
+            return new PlayerSpawnConfig(shipConfig, mainStation, godShipConfig);
+        }
+    }
 }
