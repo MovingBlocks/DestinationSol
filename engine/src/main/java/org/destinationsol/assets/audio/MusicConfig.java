079ca19fcc1cae65b474815186c770c1a8a04c1d
unknown <41499871+Torpedo99@users.noreply.github.com>
2018-11-23 10:32:50 +0530

    Added changes to MusicConfig.java and removed test music from core to another module

--- engine/src/main/java/org/destinationsol/assets/audio/MusicConfig.java
+++ engine/src/main/java/org/destinationsol/assets/audio/MusicConfig.java
@@ -23,6 +23,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Extracts music files from the musicConfig.json and stores them in a music map.
+ *
+ * A music map contains 2 lists, "gameMusic" and "menuMusic" with the respective music files, it is then passed on to OggMusicManager to be played.
+ */
 public class MusicConfig {
     public final String moduleName;
     public final Map<String, List<String>> musicMap;
@@ -39,15 +44,15 @@ public class MusicConfig {
         JSONArray menuMusicArray = config.getJSONArray("menuMusic");
         JSONArray gameMusicArray = config.getJSONArray("gameMusic");
 
-        for (Object o : menuMusicArray) {
-            if (o instanceof String) {
-                menuMusicSet.add(moduleName + ":" + o);
+        for (Object object : menuMusicArray) {
+            if (object instanceof String) {
+                menuMusicSet.add(moduleName + ":" + object);
             }
         }
 
-        for (Object o : gameMusicArray) {
-            if (o instanceof String) {
-                gameMusicSet.add(moduleName + ":" + o);
+        for (Object object : gameMusicArray) {
+            if (object instanceof String) {
+                gameMusicSet.add(moduleName + ":" + object);
             }
         }
 
