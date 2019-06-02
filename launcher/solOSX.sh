8f2a2b1693040fede125b141e293ee2a1619b0c3
Rasmus Praestholm <Rasmus.Preastholm@wnco.com>
2019-06-02 01:24:27 -0400

    Temporary workaround for #414 - avoid splash screen on Mac (and partially elsewhere)
    Breaks use of embedded JRE on Macs which needs to be re-enabled when the splash mystery is puzzled out

--- launcher/solOSX.sh
+++ launcher/solOSX.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-lwjreOSX/bin/java -jar libs/solDesktop.jar
+# TODO: Target the embedded JRE again when it'll listen to arguments or otherwise find the magic trick to allow that
+#lwjreOSX/bin/java -XstartOnFirstThread -jar libs/solDesktop.jar -noSplash
+java -XstartOnFirstThread -jar libs/solDesktop.jar -noSplash
