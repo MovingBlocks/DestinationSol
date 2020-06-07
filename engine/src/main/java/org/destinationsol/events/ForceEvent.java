e252cdaa67197f9942365e8363e416a85e3b8b70
Isaac Lichter <isaacilichter@gmail.com>
2020-06-07 14:39:40 -0400

    Slight javadoc fix

--- engine/src/main/java/org/destinationsol/events/ForceEvent.java
+++ engine/src/main/java/org/destinationsol/events/ForceEvent.java
@@ -20,8 +20,8 @@ import org.terasology.gestalt.entitysystem.event.Event;
 
 /**
  * Event that represents a prolonged force applied to an entity, such as the impact of a collision. This does NOT
- *  * represent anything that is a sudden, short force, such as a collision. That type of occurrence is handled by
- *  * {@link ImpulseEvent}.
+ * represent anything that is a sudden, short force, such as a collision. That type of occurrence is handled by
+ * {@link ImpulseEvent}.
  */
 public class ForceEvent implements Event {
 
