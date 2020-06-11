885ea12d20be1070385d548fcae9fa7b521be1c5
Isaac Lichter <isaacilichter@gmail.com>
2020-06-10 23:52:53 -0400

    Created the basic Drawable ECS structure

---
+++ engine/src/main/java/org/destinationsol/components/DrawableComponent.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 The Terasology Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.destinationsol.components;
+
+//TODO: Rename this class "Drawable" once the currently existing Drawable interface is no longer needed
+
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import org.destinationsol.game.drawables.DrawableLevel;
+import org.terasology.gestalt.entitysystem.component.Component;
+
+/**
+ * Contains a reference to an entity's texture and drawable level.
+ */
+public final class DrawableComponent implements Component<DrawableComponent> {
+
+    /**
+     * Returns the texture that this component represents.
+     */
+    public TextureAtlas.AtlasRegion texture;
+
+    /**
+     * Represents the depth at which this drawable renders, as well as its logical grouping.
+     */
+    public DrawableLevel drawableLevel;
+
+    @Override
+    public void copy(DrawableComponent other) {
+        this.texture = other.texture;
+        this.drawableLevel = other.drawableLevel;
+    }
+}
