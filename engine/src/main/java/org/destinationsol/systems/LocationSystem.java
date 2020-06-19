53a07dd7f9b1c600921db9015c5b89e7c4015023
Isaac Lichter <isaacilichter@gmail.com>
2020-06-19 12:44:56 -0400

    Added a system for handling location update events

---
+++ engine/src/main/java/org/destinationsol/systems/LocationSystem.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2020 The Terasology Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.destinationsol.systems;
+
+import org.destinationsol.components.Location;
+import org.destinationsol.entitysystem.EventReceiver;
+import org.destinationsol.events.LocationUpdateEvent;
+import org.terasology.gestalt.entitysystem.entity.EntityRef;
+import org.terasology.gestalt.entitysystem.event.EventResult;
+import org.terasology.gestalt.entitysystem.event.ReceiveEvent;
+
+/**
+ * This system updates the location of an entity with a {@link Location} component when it receives a
+ * {@link LocationUpdateEvent}.
+ */
+public class LocationSystem implements EventReceiver {
+
+    @ReceiveEvent(components = Location.class)
+    public EventResult onLocationUpdate(LocationUpdateEvent event, EntityRef entity) {
+        if (entity.hasComponent(Location.class)) {
+            Location location = entity.getComponent(Location.class).get();
+            location.position = event.getPosition();
+            location.angle = event.getAngle();
+            location.velocity = event.getVelocity();
+            entity.setComponent(location);
+        }
+        return EventResult.CONTINUE;
+    }
+}
