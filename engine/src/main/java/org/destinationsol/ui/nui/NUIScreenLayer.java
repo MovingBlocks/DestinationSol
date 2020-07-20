2fb8edbe479fef2a38875c9d9c1c362eaeae3f5b
Benjamin Amos <benjamin.amos11@gmail.com>
2020-07-20 22:04:39 +0100

    Added JavaDoc for NUIScreenLayer.

--- engine/src/main/java/org/destinationsol/ui/nui/NUIScreenLayer.java
+++ engine/src/main/java/org/destinationsol/ui/nui/NUIScreenLayer.java
@@ -29,23 +29,50 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
 
+/**
+ * A specialised {@link UIWidget} designed to contain the widgets comprising a single UI screen (or screen layer).
+ */
 public abstract class NUIScreenLayer extends AbstractWidget {
+    /**
+     * The contents of the UI screen. It contains all of the widgets in the screen and can be queried with the
+     * {@link #find} method.
+     */
     @LayoutConfig
     protected UIWidget contents;
+    /**
+     * The focus manager assigned to the UI screen.
+     */
     protected FocusManager focusManager;
+    /**
+     * The game's NUI Manager.
+     */
     protected NUIManager nuiManager;
 
+    /**
+     * This should just render the widgets contained in the UI screen, however it can be overridden to render custom
+     * graphics as well.
+     * @param canvas the canvas to draw on
+     */
     @Override
     public void onDraw(Canvas canvas) {
         canvas.drawWidget(contents);
     }
 
+    /**
+     * This updates the widgets contained in the UI screen. It is called every cycle and can therefore be used for
+     * updating the contents of the widgets as well.
+     * @param delta the time elapsed since the last update cycle
+     */
     @Override
     public void update(float delta) {
         super.update(delta);
         contents.update(delta);
     }
 
+    /**
+     * UI Screens can not by-default become focused themselves, as the focus should be on their contents instead.
+     * @return false, by default
+     */
     @Override
     public boolean canBeFocus() {
         return false;
@@ -63,6 +90,11 @@ public abstract class NUIScreenLayer extends AbstractWidget {
         return sizeHint;
     }
 
+    /**
+     * UI Screens can potentially occupy any size, as their contents is not restricted.
+     * @param canvas the canvas to render to
+     * @return the maximum possible size (Integer.MAX_VALUE, Integer.MAX_VALUE).
+     */
     @Override
     public Vector2i getMaxContentSize(Canvas canvas) {
         return new Vector2i(Integer.MAX_VALUE, Integer.MAX_VALUE);
@@ -81,6 +113,12 @@ public abstract class NUIScreenLayer extends AbstractWidget {
         return Arrays.asList(contents).iterator();
     }
 
+    /**
+     * This manages by-default the escape key closing the UI screen, however it can be overridden for more specific
+     * behaviour, such (for example) as pressing a certain key to trigger a sub-menu.
+     * @param event the key event generated
+     * @return true, if the event should be consumed, otherwise false
+     */
     @Override
     public boolean onKeyEvent(NUIKeyEvent event) {
         if (escapeCloses() && event.getState() == ButtonState.UP && event.getKey() == Keyboard.Key.ESCAPE) {
@@ -91,32 +129,64 @@ public abstract class NUIScreenLayer extends AbstractWidget {
         return super.onKeyEvent(event);
     }
 
+    /**
+     * Called to initialise the UI screen, allowing it to register widget callbacks and assign widget values
+     * programmatically. This is called whenever the UI screen becomes visible.
+     */
     public void initialise() {
     }
 
+    /**
+     * Called when the UI screen is removed from display.
+     */
     public void onRemoved() {
     }
 
+    /**
+     * States if the inputs received by this UI screen should not be received by other UI screens underneath.
+     * @return true if inputs should be blocked, otherwise false
+     */
     public boolean isBlockingInput() {
         return false;
     }
 
+    /**
+     * Is this UI screen closed when the escape key is pressed?
+     * @return true, if the screen should be closed
+     */
     protected boolean escapeCloses() {
         return true;
     }
 
+    /**
+     * Sets the focus manager to use.
+     * Primary usage is in {@link NUIManager#pushScreen}
+     * @param focusManager the focus manager to use.
+     */
     void setFocusManager(FocusManager focusManager) {
         this.focusManager = focusManager;
     }
 
+    /**
+     * Returns the focus manager currently assigned to this UI screen.
+     * @return the currently assigned focus manager
+     */
     protected FocusManager getFocusManager() {
         return focusManager;
     }
 
+    /**
+     * Sets the game's NUI Manager.
+     * @param nuiManager the game's NUI Manager
+     */
     void setNuiManager(NUIManager nuiManager) {
         this.nuiManager = nuiManager;
     }
 
+    /**
+     * Returns the game's NUI Manager.
+     * @return the game's NUI Manager
+     */
     protected NUIManager getNuiManager() {
         return nuiManager;
     }
