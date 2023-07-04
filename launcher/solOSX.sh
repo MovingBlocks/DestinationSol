e1070e01e4c1f3aced0a52b7580dabf7b81381f4
Benjamin Amos <benjamin.amos11@gmail.com>
2023-07-04 20:33:13 +0100

    fix(distribution): use embedded JRE on macOS again

--- launcher/solOSX.sh
+++ launcher/solOSX.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-# TODO: Target the embedded JRE again when it'll listen to arguments or otherwise find the magic trick to allow that
-#lwjreOSX/bin/java -XstartOnFirstThread -jar libs/solDesktop.jar -noSplash
-java -XstartOnFirstThread -jar libs/solDesktop.jar -noSplash
+lwjreOSX/bin/java -XstartOnFirstThread -jar libs/solDesktop.jar -noSplash
