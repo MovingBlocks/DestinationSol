a049900b74857af919303f2daaa80e6c77b2b628
Benjamin Amos <benjamin.amos11@gmail.com>
2026-07-30 17:58:36 +0100

    build!: upgrade Gradle to version 9.6.1

--- desktop/src/main/java/org/destinationsol/desktop/SolDesktop.java
+++ desktop/src/main/java/org/destinationsol/desktop/SolDesktop.java
@@ -94,7 +94,7 @@ public final class SolDesktop {
         try {
             splash = SplashScreen.getSplashScreen();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Failed to open splash screen", e);
         }
 
         boolean useSplash = (splash != null) && Stream.of(argv).noneMatch(s -> s.equals(NO_SPLASH_SCREEN));
